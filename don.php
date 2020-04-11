bf2a9b95bf1e166d36c796a3e4dd90aef3003cd2
KOU <elbehi.koutheir@gmail.com>
2020-04-11 17:34:47 +0100

    initial

---
+++ don.php
@@ -0,0 +1,25 @@
+<?php
+
+ 
+require('db.php');
+include("auth.php"); //include auth.php file on all secure pages ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Page de Don</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Dashboard</p>
+<p>Le transfert de don se fait dans cette page</p>
+<p><a href="index.php">Home</a></p>
+<a href="logout.php">Logout</a>
+
+
+<br /><br /><br /><br />
+
+</div>
+</body>
+</html>
